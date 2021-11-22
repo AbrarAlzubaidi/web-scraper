fe2694f102fdbe8c304d0cff6a322fff93889d65
AbrarAlzubaidi <abraralzobaide98@gmail.com>
2021-11-22 21:07:07 +0200

    solved

--- tests/test_web_scraper.py
+++ tests/test_web_scraper.py
@@ -1,5 +1,5 @@
 from web_scraper import __version__
-from web_scraper.Wikipedia_scraper import get_citations_needed_count, get_citations_needed_report ,URL
+from web_scraper.scraper import get_citations_needed_count, get_citations_needed_report ,URL
 
 def test_version():
     assert __version__ == '0.1.0'
